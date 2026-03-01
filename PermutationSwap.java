5f318cf5a1f478ba5387a58deadd34d7cc266173
Ayushhgi <goyal.ayush2424@gmail.com>
2026-03-01 13:11:51 +0530

    correction in PermutionSwap

--- PermutationSwap.java
+++ PermutationSwap.java
@@ -2,6 +2,14 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 public class PermutationSwap {
+    private static int gcd(int a,int b){
+        while(b!=0){
+            int temp=b;
+            b=a%b;
+            a=temp;
+        }
+        return a;
+    }
     public static void main(String []args){
         Scanner sc=new Scanner(System.in);
         int t=sc.nextInt();
@@ -11,15 +19,13 @@ public class PermutationSwap {
             for(int i=0;i<arr.length;i++){
                 arr[i]=sc.nextInt();
             }
-            
-            
-            int ans=0;
-            for(int i=0;i<n;i++){
-                if(Math.abs(arr[i]-(i+1))<=Math.floor(n/2)){
-                    ans=Math.max(ans,Math.abs(arr[i]-(i+1)));   
-                } 
+            int hcf=0;
+            for(int i=0;i<arr.length;i++){
+                int diff = Math.abs(arr[i] - (i + 1));
+                hcf = gcd(hcf, diff);
             }
-            System.out.println("answer->"+ans);
+            System.out.println(hcf);
+            
         }
         sc.close();
     }
